5b523afd771fb744d26e7c2235e244f7e4cd645a
branch10480 <toshiharu.imaeda@gmail.com>
2024-01-04 01:16:36 +0900

    Initial commit

---
+++ syntax.py
@@ -0,0 +1,45 @@
+def main():
+    print("Hello World!")
+
+    # 変数定義
+    a = 123
+    print(a)
+
+    # 割り算
+    print(10 / 3)   # 少数
+    print(10 // 3)  # 整数
+
+    # Bool
+    true = True
+    false = False
+
+    # if文
+    if false:
+        print("true")
+    else:
+        print("false")
+
+    # list
+    list = [1, 2, 3, 4, 5]
+    # 要素数の取得はlen関数
+    list.append(list[len(list)-1] + 1)
+    print(list)
+
+    # tuple
+    # 要素の変更ができない
+    tuple = (1, 2, 3, 4, 5)
+    print(tuple)
+
+    # 辞書
+    dict = {"one": 1, "two": 2, "three": 3}
+    print(dict)
+
+    print("For文")
+    for item in list:
+        print(item)
+
+
+# importしただけでは実行されないようにするためにこの判定を書く
+# このファイルが直接実行された場合は __name__ に __main__ が入る
+if __name__ == "__main__":
+    main()
