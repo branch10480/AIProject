c9ad3523cae69182f9d689aeba012208a5a8d721
branch10480 <toshiharu.imaeda@gmail.com>
2024-01-04 10:30:37 +0900

    Update

--- syntax.py
+++ syntax.py
@@ -56,6 +56,78 @@ def main():
     print("奇数")
     print(odd)
 
+    say_hello()
+    say_hello_to("Taro")
+    print(added(1))
+
+    # クラスの利用
+    calc = Calc(1)
+    calc.add(2)
+    print(calc.get())
+
+    # with構文を用いたファイルの書き込み
+    greetings = "Good morning!\nGood night!"
+    with open("greeting.txt", "w") as f:
+        f.write(greetings)
+
+    # with構文を用いたファイルの読み込み
+    with open("greeting.txt", "r") as f:
+        print(f.read())
+
+    # callメソッドの呼び出し
+    calc("Callメソッドの呼び出し")
+
+    # アンパック
+    numbers = [1, 2, 3]
+    a, b, c = numbers
+    print(a)
+    print(b)
+    print(c)
+
+    # アンパック（*）
+    numbers = [1, 2, 3, 4, 5]
+    a, b, *c = numbers
+    print(a)
+    print(b)
+    print(c)
+
+    # アンパック（**）
+    def greet(first_name, last_name):
+        print(f"Hi {first_name} {last_name}!")
+
+    person = {"first_name": "Taro", "last_name": "Yamada"}
+    greet(**person)
+
+
+# 関数定義
+def say_hello():
+    print("Hello!")
+
+
+def say_hello_to(name):
+    print("Hello " + name + "!")
+
+
+def added(num):
+    return num + 1
+
+
+class Calc:
+    def __init__(self, num):
+        self.num = num
+
+    def __call__(self, *args, **kwargs):
+        print(", ".join(args))
+
+    def add(self, num):
+        self.num += num
+
+    def sub(self, num):
+        self.num -= num
+
+    def get(self):
+        return self.num
+
 
 # importしただけでは実行されないようにするためにこの判定を書く
 # このファイルが直接実行された場合は __name__ に __main__ が入る
