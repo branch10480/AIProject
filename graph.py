3ba260cee4b84c588a13dc0e4d007b1a87af731e
branch10480 <toshiharu.imaeda@gmail.com>
2024-01-04 12:17:35 +0900

    Update

--- graph.py
+++ graph.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def main():
     # draw_graph1()
-    draw_graph2()
+    # draw_graph2()
+    draw_graph3()
 
 
 def draw_graph1():
@@ -29,6 +30,28 @@ def draw_graph2():
 
     plt.plot(x, y1, label="y1")
     plt.plot(x, y2, label="y2", linestyle="dashed")
+
+    plt.legend()
+
+    plt.show()
+
+
+def draw_graph3():
+    x = np.linspace(-4, 4)
+    y1 = 2*x+1
+    y2 = x**2-4
+    y3 = 0.5*x**3-6*x
+
+    plt.xlabel("x", size=14)
+    plt.ylabel("y", size=14)
+
+    plt.title("My Graph", size=20)
+
+    plt.grid()
+
+    plt.plot(x, y1, label="y1")
+    plt.plot(x, y2, label="y2")
+    plt.plot(x, y3, label="y3")
 
     plt.legend()
 
