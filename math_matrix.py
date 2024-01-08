85f136516b059018c080a91e47830e816986c257
branch10480 <toshiharu.imaeda@gmail.com>
2024-01-08 18:25:26 +0900

    Update

--- math_matrix.py
+++ math_matrix.py
@@ -3,20 +3,22 @@ import numpy as np
 
 def main():
     # sample1()
-    sample2()
+    # sample2()
+    # sample3()
+    sample4()
 
 
 def sample1():
-    a = np.array([[0, 1, 2, 3],     # 行列 npのarrayを使う
+    a = np.array([[0, 1, 2, 3],  # 行列 npのarrayを使う
                   [1, 2, 3, 4],
                   [2, 3, 4, 5]])
     print(a)
 
 
 def sample2():
-    a = np.array([[0, 1, 2],    # 行列A
+    a = np.array([[0, 1, 2],  # 行列A
                   [1, 2, 3]])
-    b = np.array([[2, 1],       # 行列B
+    b = np.array([[2, 1],  # 行列B
                   [2, 1],
                   [2, 1]])
 
@@ -25,6 +27,34 @@ def sample2():
     result = np.dot(a, b)
 
     print(result)
+
+
+def sample3():
+    # 入力: 3 ニューロン: 2
+    x = np.array([[0, 0.5, 1.0]])  # 入力
+    w = np.array([[-1, 1],  # 重み
+                  [1, -1],
+                  [-1, 1]])
+    print(np.dot(x, w))  # 入力と重みの積の総和
+
+
+def sample4():
+    a = np.array([[0, 1, 2],  # 2*3の行列
+                  [4, 5, 6]])
+    # 転置させる前
+    print("転置させる前")
+    print(a)
+
+    # 転置させる .T
+    print("転置させた後")
+    print(a.T)
+
+    print("--------")
+
+    b = np.array([[0, 1, 2],  # 2*3の行列 このままだと行列積 a*b ができない
+                  [4, 5, 6]])
+
+    print(np.dot(a, b.T))  # bを転置して行列積を計算する
 
 
 if __name__ == '__main__':
